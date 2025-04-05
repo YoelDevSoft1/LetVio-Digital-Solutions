c17c527744bb1142c57c0b32f79c657bf4275d5f
YoelDevSoft1 <yoeldevsoft@gmail.com>
2025-04-04 20:50:52 -0500

    a

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,11 @@
 import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
-
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 const isProd = process.env.NODE_ENV === "production";
 
 export default defineConfig({
-  // ... otras integraciones ...
-  site: "https://letvio.netlify.app", // <-- CAMBIA ESTO a tu URL de Netlify (o tu dominio final)
-  base: "/",                        // <-- CAMBIA ESTO a "/"
+  integrations: [react(), tailwind()], // Añadimos react() al array de integraciones
+  site: "https://letvio.netlify.app", // Tu URL de Netlify está bien
+  base: "/",                          // La base está correcta como "/"
 });
